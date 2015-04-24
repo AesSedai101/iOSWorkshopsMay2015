de2eef5e6080388df46db7fcd58dd50558227122
Discovery <discovery@Rosss-iMac.local>
2015-04-24 14:07:08 +0200

    Initial Commit

---
+++ contact/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  contact
+//
+//  Created by Discovery on 2015/04/24.
+//  Copyright (c) 2015 Discovery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ contact/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  contact
+//
+//  Created by Discovery on 2015/04/24.
+//  Copyright (c) 2015 Discovery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
