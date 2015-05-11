3f2fc7a2575f0d8a3e15f8f20fd2ad0eb819b8b5
Elsabe Ros <elsabe.ros@gmail.com>
2015-05-11 17:06:03 +0200

    Added edit controller and views.

---
+++ contact/EditController.h
@@ -0,0 +1,13 @@
+//
+//  EditController.h
+//  contact
+//
+//  Created by Discovery on 2015/05/11.
+//  Copyright (c) 2015 Discovery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditController : UIViewController
+
+@end
---
+++ contact/EditController.m
@@ -0,0 +1,37 @@
+//
+//  EditController.m
+//  contact
+//
+//  Created by Discovery on 2015/05/11.
+//  Copyright (c) 2015 Discovery. All rights reserved.
+//
+
+#import "EditController.h"
+
+@interface EditController ()
+
+@end
+
+@implementation EditController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
