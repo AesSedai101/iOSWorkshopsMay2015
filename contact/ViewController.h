f1eedf92f2f3087ff71adf5d4481b75e62bc9cbc
Elsabe Ros <elsabe.ros@gmail.com>
2015-05-27 19:23:54 +0200

    Initial Commit

---
+++ contact/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  contact
+//
+//  Created by Elsabe Ros on 2015/05/27.
+//  Copyright (c) 2015 Elsabe Ros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ contact/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  contact
+//
+//  Created by Elsabe Ros on 2015/05/27.
+//  Copyright (c) 2015 Elsabe Ros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
